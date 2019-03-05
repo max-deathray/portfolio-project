cd9150c21789828e8445f1a16b433a4bb6d64ccf
McRae <mpetrey@gmail.com>
2019-03-04 23:50:11 -0500

    some styling

--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class Job(models.Model):
+    title = models.CharField(max_length=100, default="title")
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    url = models.CharField(max_length=500, default="url")
+
+    # this allows the title of the blog to be displayed in the admin
+    def __str__(self):
+        return self.title
