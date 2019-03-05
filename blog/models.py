9492c19f23b61189ccf379ac63b704c5b08352fe
McRae <mpetrey@gmail.com>
2019-03-04 21:46:01 -0500

    finishing up

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,16 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
+    # this allows the title of the blog to be displayed in the admin
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
 # add the blogp app to the settings
 
 # create a migration
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+# access items from the database
+from .models import Blog
 
 
 def allblogs(request):
-    return render(request, 'blog/allblogs.html')
+    # this next line grabs all the jobs from the database
+    blogs = Blog.objects
+    # then pass a dictionary to the render function
+    return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': detailblog})
